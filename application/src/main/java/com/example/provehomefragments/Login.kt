58b1e9ad33dbaf02d1ce46187909cfa317d3aa68
brugogaia <brugogaia@users.noreply.github.com>
2021-03-09 18:15:57 +0100

    nuovo login e registrazione (inserita anche Activity iniziale il login)

---
+++ application/src/main/java/com/example/provehomefragments/Login.kt
@@ -0,0 +1,59 @@
+package com.example.provehomefragments
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.doAfterTextChanged
+import kotlinx.android.synthetic.main.login.*
+import android.content.Context
+import android.widget.CalendarView.OnDateChangeListener
+import java.io.BufferedOutputStream
+import java.io.DataOutputStream
+import java.util.*
+
+class Login : AppCompatActivity() {
+
+    var email_insert= String()
+    var password_insert= String()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        btn_login.isEnabled=false
+
+        //save email
+        email.doAfterTextChanged {
+            btn_login.isEnabled=false
+            email_insert=email.text.toString()
+            if((email.text.toString().isNotEmpty()) && (password.text.toString().isNotEmpty())){
+                btn_login.isEnabled=true
+            }
+        }
+
+        //save password
+        password.doAfterTextChanged {
+            btn_login.isEnabled=false
+            password_insert=password.text.toString()
+            if((email.text.toString().isNotEmpty()) && (password.text.toString().isNotEmpty())){
+                btn_login.isEnabled=true
+            }
+        }
+
+        //control with database
+
+        //Click Login
+        btn_login.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            this.finish()
+        }
+
+        //Click Register
+        btn_register.setOnClickListener{
+            val j = Intent(this, Register::class.java)
+            startActivity(j)
+        }
+    }
+}
---
+++ application/src/main/java/com/example/provehomefragments/Register.kt
@@ -0,0 +1,16 @@
+package com.example.provehomefragments
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Register: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_registration)
+
+
+
+
+    }
+}
