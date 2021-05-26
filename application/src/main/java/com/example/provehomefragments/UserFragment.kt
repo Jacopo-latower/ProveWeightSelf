aa711dfc8d710e29afdb950d4e8eef22995212b2
brugogaia <brugogaia@users.noreply.github.com>
2021-05-26 11:08:59 +0200

    modificato user con calcolo bmi e cambiato il bottone nelle ricette e negli allenamenti

--- application/src/main/java/com/example/provehomefragments/UserFragment.kt
+++ application/src/main/java/com/example/provehomefragments/UserFragment.kt
@@ -2,6 +2,7 @@ package com.example.provehomefragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Debug
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.user_frag_layout.*
+
 
 interface UserFragObserver{
     fun userFragCreatedNotify()
@@ -29,6 +31,8 @@ class UserFragment : Fragment() {
 
         val nameTv = activity?.findViewById<TextView>(R.id.usernameTextView)
         val userData = RepositoryManager.instance.loadUserData()
+        val bmi = activity?.findViewById<TextView>(R.id.bmi)
+        val bmiCondition= activity?.findViewById<TextView>(R.id.bmi_condition)
 
         nameTv?.text = ("${userData["name"].toString()} ${userData["surname".toString()]}")
 
@@ -39,6 +43,20 @@ class UserFragment : Fragment() {
             btnLogout.isEnabled = false
             logout()
         }
+
+        //Calcolo BMI
+        val hx2 = Integer.parseInt(userData["height"].toString())*Integer.parseInt(userData["height"].toString())
+        val lastweight= 70 //al posto del 70 ci dovrebbe essere l'ultimo peso salvato
+        val bmiCalculate = (lastweight*10000)/hx2
+
+        bmi?.text= (bmiCalculate.toString())
+
+        if(bmiCalculate<18) {bmiCondition?.text = getString(R.string.below18)}
+        else if (bmiCalculate in 18..24) {bmiCondition?.text= getString(R.string.between1825)}
+        else if (bmiCalculate in 25..29) {bmiCondition?.text= getString(R.string.between2530)}
+        else if (bmiCalculate in 30..34) {bmiCondition?.text= getString(R.string.between3035)}
+        else if (bmiCalculate in 35..39) {bmiCondition?.text= getString(R.string.between3540)}
+        else if (bmiCalculate>=40) {bmiCondition?.text= getString(R.string.above40)}
     }
 
     private fun logout(){
